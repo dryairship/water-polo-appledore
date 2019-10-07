41d540399b8500e6fcb17174377869466064d2ef
Priydarshi Singh <dryairship@gmail.com>
2019-10-07 21:26:49 +0530

    :tada: Initial commit

---
+++ src/main/java/org/dryairship/waterpoloappledore/App.java
@@ -0,0 +1,13 @@
+package org.dryairship.waterpoloappledore;  
+
+/**
+ * Main App
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "App coming soon." );
+    }
+}
